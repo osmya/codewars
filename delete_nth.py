2a789338c8e0e16657545130899f0012f378b190
osmya <45291785+osmya@users.noreply.github.com>
2019-06-19 13:05:31 +0100

    Update delete_nth.py

--- delete_nth.py
+++ delete_nth.py
@@ -5,14 +5,24 @@ def delete_nth(order, max_e):
     >>> delete_nth([1,1,3,3,7,2,2,2,2], 3)
     [1, 1, 3, 3, 7, 2, 2, 2]
     """
-    new_order = []
+    resp = []
     for i in order:
             k = 0
-            if new_order.count(i) == max_e:
+            if resp.count(i) == max_e:
                 k = max_e + 1
             while k <= max_e:
-                new_order += [i]
+                resp += [i]
                 k += 1
-                if i in new_order:
+                if i in resp:
                     break
     return new_order
+
+
+# alt solution
+
+def delete_nth_b(order,max_e):
+    resp = []
+    for i in order:
+        if resp.count(i) < max_e:
+            resp.append(i)
+    return resp
